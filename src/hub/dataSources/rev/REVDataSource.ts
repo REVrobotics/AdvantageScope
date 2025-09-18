9e34b48e0a98cb50dba733865f7c2ba6e24871f9
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2025-09-18 16:31:30 -0500

    Connect to WebSocket and parse incoming data

--- src/hub/dataSources/rev/REVDataSource.ts
+++ src/hub/dataSources/rev/REVDataSource.ts
@@ -1,14 +1,18 @@
 import { LiveDataSource, LiveDataSourceStatus } from "../LiveDataSource";
 import Log from "../../../shared/log/Log";
+import { REVTelemetryClient } from "./REVTelemetryClient";
 
 export default class REVDataSource extends LiveDataSource {
+  client: REVTelemetryClient | undefined;
+
   override connect(
     address: string,
     statusCallback: (status: LiveDataSourceStatus) => void,
-    outputCallback: (log: Log, timeSupplier: () => number) => void,
+    outputCallback: (log: Log, timeSupplier: () => number) => void
   ) {
     super.connect(address, statusCallback, outputCallback, false);
     this.log = new Log();
-
+    this.client = new REVTelemetryClient(this.log);
+    this.client.connect();
   }
 }
--- src/hub/dataSources/rev/REVTelemetryClient.ts
+++ src/hub/dataSources/rev/REVTelemetryClient.ts
@@ -1,4 +1,63 @@
+import Log from "../../../shared/log/Log";
 
-export class RevTelemetryClient {
+export class REVTelemetryClient {
+  log: Log;
 
+  constructor(log: Log) {
+    this.log = log;
+  }
+
+  connect() {
+    let socket = new WebSocket("ws://127.0.0.1:59977/v1/ws/status");
+
+    socket.addEventListener("open", event => {
+      let key = crypto.randomUUID();
+      console.log(key);
+
+      socket.send(JSON.stringify({ key: key }));
+    });
+
+    socket.addEventListener("message", event => {
+      let statusData = JSON.parse(event.data);
+
+      if(statusData !== undefined && statusData !== null) {
+        this.handleStatusFrame(statusData);
+      }
+    });
+  }
+
+  handleStatusFrame(frameData: any) {
+    let descriptor = frameData.descriptor;
+    let timestamp = Date.now();
+
+    for(let statusFrameKey in frameData) {
+      let statusFrame = frameData[statusFrameKey];
+
+      if(statusFrame === undefined || statusFrame === null) {
+        continue;
+      }
+
+      if(typeof statusFrame !== "object") {
+        continue;
+      }
+
+      for(let fieldKey in statusFrame) {
+        let value = statusFrame[fieldKey];
+        let fieldName = `${descriptor}.${statusFrameKey}.${fieldKey}`;
+
+        if(value !== undefined && value !== null) {
+          console.log(`Status: ${fieldName}: ${value}`);
+
+          switch(typeof value) {
+            case "number":
+              this.log.putNumber(fieldName, timestamp, value);
+              break;
+            case "boolean":
+              this.log.putBoolean(fieldName, timestamp, value);
+              break;
+          }
+        }
+      }
+    }
+  }
 }
