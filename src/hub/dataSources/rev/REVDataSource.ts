17e18fb766f7938685e9fd6ccb93819753a2f78c
LandryNorris <37489471+LandryNorris@users.noreply.github.com>
2025-09-18 17:23:57 -0500

    Call methods to output data

--- src/hub/dataSources/rev/REVDataSource.ts
+++ src/hub/dataSources/rev/REVDataSource.ts
@@ -4,6 +4,7 @@ import { REVTelemetryClient } from "./REVTelemetryClient";
 
 export default class REVDataSource extends LiveDataSource {
   client: REVTelemetryClient | undefined;
+  liveZeroTime: number | undefined;
 
   override connect(
     address: string,
@@ -17,6 +18,25 @@ export default class REVDataSource extends LiveDataSource {
   }
 
   onMessage() {
-    this.status = LiveDataSourceStatus.Active;
+    this.setStatus(LiveDataSourceStatus.Active);
+
+    if (this.liveZeroTime === undefined) {
+      this.liveZeroTime = new Date().getTime() / 1000;
+    }
+
+    if (this.log === null) {
+      return;
+    }
+
+    // Run output callback
+    if (this.outputCallback !== null) {
+      this.outputCallback(this.log, () => {
+        if (this.log && this.liveZeroTime !== undefined) {
+          return new Date().getTime() / 1000 - this.liveZeroTime + this.log.getTimestampRange()[0];
+        } else {
+          return 0;
+        }
+      });
+    }
   }
 }
